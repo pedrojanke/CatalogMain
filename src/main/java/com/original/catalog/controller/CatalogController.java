b503a962603d9c790862b430e396d629edf10e7e
Pedro Henrique Janke <90357226+pedrojanke@users.noreply.github.com>
2024-06-12 19:11:50 -0300

    finish controller

--- src/main/java/com/original/catalog/controller/CatalogController.java
+++ src/main/java/com/original/catalog/controller/CatalogController.java
@@ -1,10 +1,15 @@
 package com.original.catalog.controller;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,5 +34,40 @@ public class CatalogController {
         }
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(new ApiResponse<Catalog>("Created", catalogService.createCatalog(dto)));
+    }
+
+    @GetMapping
+    public ResponseEntity<ApiResponse<List<Catalog>>> listCatalog() {
+        List<Catalog> listCatalog = catalogService.listCatalog();
+        return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse<List<Catalog>>("Ok", listCatalog));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ApiResponse<Catalog>> findCatalog(@PathVariable String id) {
+        Catalog catalog = catalogService.findCatalog(id);
+        if (catalog == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse<Catalog>("Ok", catalog));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ApiResponse> deleteCatalog(@PathVariable String id) {
+        Catalog catalog = catalogService.findCatalog(id);
+        if (catalog == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+        catalogService.deleteCatalog(id);
+        return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse("Deleted"));
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<ApiResponse> updateCatalog(@PathVariable String id, @RequestBody CatalogDto dto) {
+        Catalog catalog = catalogService.findCatalog(id);
+        if (catalog == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiResponse("Espécie não encontrada."));
+        }
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new ApiResponse<Catalog>("Updated", catalogService.updateCatalogById(id, dto)));
     }
 }
