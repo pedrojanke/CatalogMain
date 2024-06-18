c2bca6095c508fc67acc49318c8394101e6053fc
Pedro Henrique Janke <90357226+pedrojanke@users.noreply.github.com>
2024-06-18 18:53:55 -0300

    ajustes

--- src/main/java/com/original/catalog/service/CatalogService.java
+++ src/main/java/com/original/catalog/service/CatalogService.java
@@ -1,6 +1,5 @@
 package com.original.catalog.service;
 
-import java.io.IOException;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
@@ -11,12 +10,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.original.catalog.dto.CatalogDto;
 import com.original.catalog.entities.Catalog;
 import com.original.catalog.repository.CatalogRepository;
-import com.original.catalog.tests.IdWithName;
 
 import jakarta.transaction.Transactional;
 
@@ -76,50 +72,6 @@ public class CatalogService {
             return catalog;
         } catch (Exception e) {
             throw new RuntimeException(e.getMessage());
-        }
-    }
-
-    public List<Catalog> listCatalogName() {
-        try {
-            List<Catalog> catalogs = catalogRepository.findAll();
-    
-            for (Catalog catalog : catalogs) {
-                catalog.setCategory(getIdWithNameFromEndpoint("http://localhost:8081/api/v1/category/", catalog.getCategoryId()));
-                catalog.setMedia(getIdWithNameFromEndpoint("http://localhost:8081/api/v1/media/", catalog.getMediaId()));
-                catalog.setMediaType(getIdWithNameFromEndpoint("http://localhost:8081/api/v1/mediatype/", catalog.getMediaTypeId()));
-                catalog.setClassification(getIdWithNameFromEndpoint("http://localhost:8081/api/v1/classification/", catalog.getClassificationId()));
-                catalog.setParticipant(getIdWithNameFromEndpoint("http://localhost:8081/api/v1/participant/", catalog.getParticipantId()));
-            }
-    
-            return catalogs;
-        } catch (Exception e) {
-            throw new RuntimeException("Erro ao listar catálogos: " + e.getMessage());
-        }
-    }
-
-    // Compara o ID e pega o nome que representa cada ID dos endpoints
-    private IdWithName getIdWithNameFromEndpoint(String baseUrl, String id) {
-        try {
-            ResponseEntity<String> response = restTemplate.getForEntity(baseUrl + id, String.class);
-            if (response.getStatusCode().is2xxSuccessful()) {
-                ObjectMapper objectMapper = new ObjectMapper();
-                JsonNode root = objectMapper.readTree(response.getBody());
-                JsonNode data = root.get("data");
-                if (data != null) {
-                    JsonNode nameNode = data.get("name");
-                    if (nameNode != null) {
-                        String name = nameNode.asText();
-                        return new IdWithName(name, id);
-                    }
-                }
-                return new IdWithName("Nome não encontrado", id);
-            } else {
-                return new IdWithName("Nome não encontrado", id);
-            }
-        } catch (HttpClientErrorException.NotFound e) {
-            return new IdWithName("ID não encontrado", id);
-        } catch (IOException e) {
-            return new IdWithName("Erro ao processar resposta", id);
         }
     }
 
